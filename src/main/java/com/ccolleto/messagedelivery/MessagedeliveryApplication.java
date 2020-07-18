1807f67794d8d360b5c632b2ea888cf2a6220638
Carol Colleto <ccolleto@lixar.com>
2020-07-18 12:09:17 -0300

    Initial repo created by Spring Initializr

---
+++ src/main/java/com/ccolleto/messagedelivery/MessagedeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.ccolleto.messagedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagedeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagedeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccolleto/messagedelivery/MessagedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccolleto.messagedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
